f348f969fcc2b0001a218d2b6c3d736dbde174f0
jakubskiba <jakubskibakrk@gmail.com>
2018-03-01 23:09:15 +0100

    update bash run script

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
-cd target/classes
-java com.codecool.App
+if [ ! -f target/mtfc-1.0-SNAPSHOT-jar-with-dependencies.jar ]; then
+    sh ./install.sh
+fi
+java -jar target/mtfc-1.0-SNAPSHOT-jar-with-dependencies.jar
